c0da469c6596a52a5b089769a5354c4e656e34c7
Khaleed Uddin <uddin1786@gmail.com>
2022-09-07 18:41:07 -0400

    deployment

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,5 +1,8 @@
 export default function Home() {
   return (
-    <div className="anabrolic">Welcome to the AnaBrolic 💪 Calorie Tracker App!</div>
+    <div className="Home"> 
+    <h6> Welcome to the AnaBrolic 💪 Calorie Tracker App!</h6>
+    <img src="https://is5-ssl.mzstatic.com/image/thumb/Purple118/v4/67/fd/96/67fd96d4-20c8-9877-7481-1693b0b0623e/source/200x200bb.jpg" alt="some pic lol"></img>
+  </div>
   )
 }

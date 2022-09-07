7bfc5b126db0924d6c4b963663bca093aac06d4b
Khaleed Uddin <uddin1786@gmail.com>
2022-09-07 18:45:16 -0400

    deplopyment

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <div className='NavBar'>
             <nav>
-                <Link className='anabrolic' to='/'>AnaBrolic 💪 Calorie Tracker</Link>
+                <Link className='anabrolic' to='/'>AnaBrolic 💪 </Link>
                 <button className='foodButton'><Link to='/foods'>Foods</Link></button>
                 <button className='newButton'><Link to='/foods/new'>Add Foods</Link> </button>
             </nav>

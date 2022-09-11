d7d43d1017d09baa7d2c46e37b2d8eb02498e759
Khaleed Uddin <uddin1786@gmail.com>
2022-09-11 09:52:21 -0400

    dynamic app listen to port for heroku

--- back-end/server.js
+++ back-end/server.js
@@ -4,6 +4,6 @@ require("dotenv").config();
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log(`Flexin on port: ${PORT}!!`);
 });
